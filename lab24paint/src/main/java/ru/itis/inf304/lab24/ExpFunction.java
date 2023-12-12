675f5f1e4bd733da47fa3c7dc7da10819ba3224b
ksenikeev <ksenikeev@mail.ru>
2023-12-12 21:37:55 +0300

    Изменена функция для отображения

--- lab24paint/src/main/java/ru/itis/inf304/lab24/ExpFunction.java
+++ lab24paint/src/main/java/ru/itis/inf304/lab24/ExpFunction.java
@@ -1,9 +1,8 @@
 package ru.itis.inf304.lab24;
 
 public class ExpFunction implements IFunction {
-
     @Override
     public double calculate(double x) {
-        return x;
+        return x * x - 10;
     }
 }
